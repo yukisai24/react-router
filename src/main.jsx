5feb93cdb0d895e27145d95d309674fd306a8066
jongwoo_sohn <yukisai24@naver.com>
2024-04-21 13:37:12 +0900

    action에 함수를 할당하면 해당 함수에 params를 인자로 Form의 버튼에 온클릭이벤트로 post요청이 날아간다

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root, { loader as rootLoader } from "./routes/root";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
-
+import Contact, { loader as contactLoader } from "./routes/contact";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
       },
     ],
   },
